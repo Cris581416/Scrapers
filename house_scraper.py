c53a7670ed3eba38fbed50f19525677bf78b0885
Cris581416 <cvleazquez581416@gmail.com>
2020-11-06 22:18:56 -0800

    Add and update houses.json file with addresses and info

--- house_scraper.py
+++ house_scraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from os import path
+import json
 import time
 
 driver_path = path.join(path.dirname(path.abspath(__file__)), "chromedriver")
@@ -29,11 +30,29 @@ table_mode = driver.find_element_by_xpath("/html/body/div[1]/div[6]/div[2]/div[1
 
 time.sleep(1)
 
-property_list = []
+info_format = ["location", "price", "beds", "baths"]
+property_dict = {}
 property_table = driver.find_element_by_css_selector("tbody")
 for row_num, row in enumerate(property_table.find_elements_by_tag_name("tr")):
-    property_list.append(row.find_elements_by_tag_name("td")[1].text)
+    data = row.find_elements_by_tag_name("td")
+    key = data[1].text
+    property_dict[key] = {}
+    for element_num, element in enumerate(data[3:6]):
+        property_dict[key][info_format[element_num]] = element.text
+    print(property_dict[key])
 
-print(property_list)
+with open('houses.json', 'r') as file:
+    listed_props = json.load(file)
+
+for property in property_dict:
+    if property in listed_props["houses"]:
+        print("House already in property_dict")
+        continue
+
+    print(f"New Property: {property}")
+    listed_props["houses"][property] = property_dict[property]
+
+with open('houses.json', 'w') as file:
+    json.dump(listed_props, file, indent=4)
 
 driver.close()
